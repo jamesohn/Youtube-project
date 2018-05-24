1842d3540fd5c705acdc3c55dcb40e5f3ad8396a
jamesohn <jamesohn247@gmail.com>
2018-05-24 20:13:01 +0900

    New locale analysis

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 /* load modules */
 const config = require('config'),
   fs = require('fs'),
-  AuthorChannelId = require('./lib/youtube_authorChannelId_node.js'),
+  AuthorChannelId = require('./lib/authorChannelId.js'),
   authorChannelId = new AuthorChannelId(config.commentThreads),
-  crawler = require('./lib/crawler0.js'),
   stuff = require('./lib/stuff.js'),
   promisify = require('util').promisify,
   cluster = require('cluster'),
@@ -13,12 +12,14 @@ const config = require('config'),
   numCPUs = require('os').cpus().length,
   mkdirp = promisify(require('mkdirp'))
 
-let channelRaw = ['UCV0qA-eDDICsRR9rPcnG7tw'];
-let channelList = [];
+let channelRaw = fs.readFileSync('./filtered.csv', 'utf8').split(/\r?\n/),
+// ['UC4xKdmAXFh4ACyhpiQ_3qBw'],
+  channelList = [];
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
   for (let channel of channelRaw) {
-    channelList.push(channel)
+    // channelList.push(channel)
+    channelList.push(channel.substring(31))
   }
 }
 const main = async (channelList) => {
@@ -27,7 +28,7 @@ const main = async (channelList) => {
       console.log('this is ' + channel);
       pqueueAuthorChannelId.add(() => authorChannelId.collect(channel))
       .then(function(result) {
-        pqueuecrawler.add(() => crawler.collect(channel, result))
+        console.log(result);
       })
     } catch (e) {
       console.log(e);
